52015d4e78b6b90ae605b7c33a70fab798041d1e
vdunlop <148563390+vdunlop@users.noreply.github.com>
2023-12-11 17:05:16 -0600

    Added images limited to being displayed once every 3 chapters. Limit is a const that can be changed

--- develop/js/script.js
+++ develop/js/script.js
@@ -1,3 +1,9 @@
+// Consts
+const NUM_PAGES_B4_IMG = 3;
+const PAGES_ENTERED_INIT = 0;
+
+// Global variables
+var pagesEntered = PAGES_ENTERED_INIT; // Start incrementing in next chapter
 
 // Selectors
 var pageAPI = $('.page-api');
@@ -26,7 +32,7 @@ if (apiKey) {
 }
 
 // Test API for validity, Store API key on submission
-submitAPI.click(function(event) {
+submitAPI.click(function (event) {
     event.preventDefault();
     apiKey = inputAPI.val();
 
@@ -40,30 +46,33 @@ submitAPI.click(function(event) {
         },
         body: JSON.stringify({
             model: 'gpt-3.5-turbo',
-            messages: [{role: "system", content: prompt}],
+            messages: [{ role: "system", content: prompt }],
             max_tokens: 25
         })
     })
-    .then(response => {
-        if (response.ok) {
-            // API key is valid, go ahead and set it to local storage
-            localStorage.setItem('apiKey', apiKey); // sets the API key in localStorage
-            pageAPI.hide(); // hides the form
-            pageStartAdventure.show(); // shows the story start question
-        } else {
-            $('<p style="color:red">API Key is not valid. Please try again.</p>').appendTo(pageAPI); // shows error message, might need to style later
-            inputAPI.val(''); // clears the API input form field
-        }
-    })
-   .catch(error => {
-        console.error('Error:', error);
-        $('<p style="color:red">Something went wrong, please try again</p>').appendTo(pageAPI); // in case something else goes wrong during submission
-   });
+        .then(response => {
+            if (response.ok) {
+                // API key is valid, go ahead and set it to local storage
+                localStorage.setItem('apiKey', apiKey); // sets the API key in localStorage
+                pageAPI.hide(); // hides the form
+                pageStartAdventure.show(); // shows the story start question
+            } else {
+                $('<p style="color:red">API Key is not valid. Please try again.</p>').appendTo(pageAPI); // shows error message, might need to style later
+                inputAPI.val(''); // clears the API input form field
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            $('<p style="color:red">Something went wrong, please try again</p>').appendTo(pageAPI); // in case something else goes wrong during submission
+        });
 });
 
 // Handle the start of the adventure
-formStartAdventure.submit(function(event) {
+formStartAdventure.submit(function (event) {
     event.preventDefault();
+
+    // Initialize
+    pagesEntered = PAGES_ENTERED_INIT;
     pageStartAdventure.hide(); // on submit, hide the question and input form
     var userResponse = $('#page-start-adventure-input').val(); // this is the user response for the first question
     generateStory(userResponse, false); // false indicating it's the first chapter, true means its a looping subsequent chapter function
@@ -71,8 +80,11 @@ formStartAdventure.submit(function(event) {
 });
 
 // Handle subsequent chapters
-formNextChapter.submit(function(event) {
+formNextChapter.submit(function (event) {
     event.preventDefault();
+    // Keep track of how many chapters, pages the user has entered
+    pagesEntered++;
+
     var userResponse = $('#page-next-chapter-input').val(); // this is the user response for all subsequent questions
     generateStory(userResponse, true); // true indicating it's not the first chapter
     $('#page-next-chapter-input').val('');
@@ -80,9 +92,9 @@ formNextChapter.submit(function(event) {
 
 // Function to generate story text
 function generateStory(userResponse, isNextChapter) {
-    var prompt = isNextChapter ? 
-                    `Continue the story: ${userResponse}, second-person creative narrative with dramatic twists, make it about 75 words, use the present tense.` : 
-                    `Start a story with: ${userResponse}, second-person creative narrative with dramatic twists, make it about 75 words, use the present tense.`;
+    var prompt = isNextChapter ?
+        `Continue the story: ${userResponse}, second-person creative narrative with dramatic twists, make it about 75 words, use the present tense.` :
+        `Start a story with: ${userResponse}, second-person creative narrative with dramatic twists, make it about 75 words, use the present tense.`;
 
     // The gpt text call to Open AI                   
     fetch('https://api.openai.com/v1/chat/completions', {
@@ -93,21 +105,25 @@ function generateStory(userResponse, isNextChapter) {
         },
         body: JSON.stringify({
             model: 'gpt-3.5-turbo',
-            messages: [{role: "system", content: prompt}],
-            max_tokens: 200 
+            messages: [{ role: "system", content: prompt }],
+            max_tokens: 200
         })
     })
-    .then(response => response.json())
-    .then(data => {
-        var storyText = data.choices[0].message.content.trim(); // this is where the response is stored in data
-        gptText.text(storyText); // show the response text in the gptText element
-        pageNextChapter.show(); // show the text on the page
-        generateImage(storyText); // generate the dall-e image function
-    });
+        .then(response => response.json())
+        .then(data => {
+            var storyText = data.choices[0].message.content.trim(); // this is where the response is stored in data
+            gptText.text(storyText); // show the response text in the gptText element
+            pageNextChapter.show(); // show the text on the page
+
+            // Generate image every NUM_PAGES_B4_IMG chapters
+            if (!(pagesEntered % NUM_PAGES_B4_IMG)) {
+                generateImage(storyText); // generate the dall-e image function
+            }
+        });
 }
 
- // Function to generate image, using the text from the generated story
- function generateImage(storyText) {
+// Function to generate image, using the text from the generated story
+function generateImage(storyText) {
     dalleImage.hide(); // hide the previous image
     loadingSpinner.show() // show a CSS loading spinner, may want to add "loading image, please wait 10 seconds"
 
@@ -125,17 +141,17 @@ function generateStory(userResponse, isNextChapter) {
             size: '1024x1024' // the size of the image, i wonder if a smaller size takes less tokens?
         })
     })
-    .then(response => response.json())
-    .then(data => {
-        // console.log(data); for development testing
-        loadingSpinner.hide(); // once the image is generated, hide the spinner
-        var imageUrl = data.data[0].url; // this is the image url that we'll feed the img container
-        dalleImage.attr('src', imageUrl); // attaching the image url to the src attribute of this image element
-        dalleImage.show(); // show the image. right now it briefly shows the previous image, so this needs to be fixed
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
+        .then(response => response.json())
+        .then(data => {
+            // console.log(data); for development testing
+            loadingSpinner.hide(); // once the image is generated, hide the spinner
+            var imageUrl = data.data[0].url; // this is the image url that we'll feed the img container
+            dalleImage.attr('src', imageUrl); // attaching the image url to the src attribute of this image element
+            dalleImage.show(); // show the image. right now it briefly shows the previous image, so this needs to be fixed
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
 }
 
 
